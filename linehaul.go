301a981dc78e2fdb78450d84bbeb582d6c26ee67
Steve Gray <steve@eventualconsistency.net>
2016-11-03 21:00:32 +1000

    Initial vendoring setup.

--- linehaul.go
+++ linehaul.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-        "fmt"
+	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    fmt.Print("Hello World")
+	fmt.Print("Hello World\n")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and server on 0.0.0.0:8080
 }
